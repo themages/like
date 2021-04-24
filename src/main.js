2508a5ea2971399859a8bdf503ea086d8f71b81b
zengxingqi <me@zengxingqi.com>
2021-04-24 16:51:29 +0800

    测试

--- src/main.js
+++ src/main.js
@@ -2,5 +2,5 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
+// 测试
 createApp(App).use(store).use(router).mount("#app");
