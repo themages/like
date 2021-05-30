8a9765ede5df7284fcce5215f796ee4e9a73797e
zengxingqi <me@zengxingqi.com>
2021-05-30 22:43:11 +0800

    feat: demo

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,12 @@
   <router-view />
 </template>
 <script>
-// @ is an alias to /src
-
+// 外部模块
+// 1. npm 2.组件 3. 类方法
+// 内部模块
+// 1. 组件 2. vuex vue指令 3. 类方法 工具函数
+import { postData } from "@/fetch/index";
+import { getUserDevices, getUserMedia } from "@/mediaSDK/sdk/index";
 export default {
   name: "App",
   data() {
@@ -22,23 +26,39 @@ export default {
   },
   methods: {
     getDevices() {
-      const devices = navigator.mediaDevices.enumerateDevices();
-      devices
-        .then((devices) => {
-          console.log("devices: %O", devices);
-        })
-        .catch((err) => {
-          console.error("devices err: %O", err);
-        });
+      getUserDevices({
+        handler(err, device) {
+          if (err) console.error("devices err: %O", err);
+          console.log("devices: %O", device);
+        },
+      });
     },
     getUserMedia() {
       const constraints = {
         audio: false,
         video: true,
       };
-      const devices = navigator.mediaDevices.getUserMedia(constraints);
-      devices
-        .then((stream) => {
+      getUserMedia({
+        constraints,
+        handler(err, stream) {
+          if (err) {
+            if (err.name === "ConstraintNotSatisfiedError") {
+              this.errorMsg(
+                "The resolution " +
+                  constraints.video.width.exact +
+                  "x" +
+                  constraints.video.width.exact +
+                  " px is not supported by your device."
+              );
+            } else if (err.name === "PermissionDeniedError") {
+              this.errorMsg(
+                "Permissions have not been granted to use your camera and " +
+                  "microphone, you need to allow the page access to your devices in " +
+                  "order for the demo to work."
+              );
+            }
+            this.errorMsg("getUserMedia error: " + err.name, err);
+          }
           const videoTracks = stream.getVideoTracks();
           const player = document.querySelector("video#player");
           console.log("Got stream with constraints:", constraints);
@@ -49,30 +69,31 @@ export default {
           window.stream = stream;
           player.srcObject = stream;
           console.log(player);
-        })
-        .catch((error) => {
-          if (error.name === "ConstraintNotSatisfiedError") {
-            this.errorMsg(
-              "The resolution " +
-                constraints.video.width.exact +
-                "x" +
-                constraints.video.width.exact +
-                " px is not supported by your device."
-            );
-          } else if (error.name === "PermissionDeniedError") {
-            this.errorMsg(
-              "Permissions have not been granted to use your camera and " +
-                "microphone, you need to allow the page access to your devices in " +
-                "order for the demo to work."
-            );
-          }
-          this.errorMsg("getUserMedia error: " + error.name, error);
-        });
+        },
+      });
     },
     errorMsg(msg, error) {
       if (typeof error !== "undefined") {
         console.error("错误：%O, %O", error, msg);
       }
+    },
+    fetchServeData(sdp) {
+      const data = {
+        api: "http://tv.canicode.cn:1985/rtc/v1/publish/",
+        streamurl: "webrtc://tv.canicode.cn/live/livestream",
+        clientip: null,
+        sdp,
+      };
+      postData({
+        url: data.api,
+        data,
+        handler(err, data) {
+          console.log("Got answer: %O", data);
+          if (err || data.code) {
+            return;
+          }
+        },
+      });
     },
   },
 };
