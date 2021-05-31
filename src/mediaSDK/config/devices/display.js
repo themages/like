90ca4a2b1d7d44c0d4bb0dbcb2997cd709d1cb79
zengxingqi <me@zengxingqi.com>
2021-05-31 17:17:21 +0800

    feat: name

--- src/mediaSDK/config/devices/display.js
+++ src/mediaSDK/config/devices/display.js
@@ -2,11 +2,11 @@
  * @Author: 曾星旗 <me@zengxingqi.com>
  * @Date: 2021-04-27 00:04:55
  * @LastEditors: 曾星旗 <me@zengxingqi.com>
- * @LastEditTime: 2021-05-30 19:52:49
+ * @LastEditTime: 2021-05-31 17:17:06
  * @Description: 用户显示器或窗口
  * @FilePath: /like/src/mediaSDK/config/devices/display.js
  */
-export default function constraints(obj) {
+export default function getDisplayMedia(obj) {
   return navigator.mediaDevices
     .getDisplayMedia(obj.constraints)
     .then((display) => {
