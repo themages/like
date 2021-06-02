dc8a67b7681ef04bdf3fb35ac17a4a72ef9539d8
zengxingqi <me@zengxingqi.com>
2021-06-02 19:15:27 +0800

    add: http module

--- src/http/index.js
+++ src/http/index.js
@@ -2,18 +2,35 @@
  * @Author: 曾星旗 <me@zengxingqi.com>
  * @Date: 2021-05-30 22:07:32
  * @LastEditors: 曾星旗 <me@zengxingqi.com>
- * @LastEditTime: 2021-06-02 18:01:34
+ * @LastEditTime: 2021-06-02 19:12:33
  * @Description: http 请求响应
  * @FilePath: /like/src/http/index.js
  */
 import Request from "./request";
 
+export function Get(obj = {}) {
+  const { url, data, handler, responseType } = obj;
+  return Request({
+    url,
+    params: data,
+    method: "GET",
+    responseType, // 选项包括: 'arraybuffer', 'document', 'json', 'text', 'stream', 浏览器专属：'blob'
+  })
+    .then((res) => {
+      handler(null, res);
+    })
+    .catch((err) => {
+      handler(err);
+    });
+}
+
 export function Post(obj = {}) {
-  const { url, data, handler } = obj;
+  const { url, data, handler, responseType } = obj;
   return Request({
     url,
     data,
     method: "POST",
+    responseType,
   })
     .then((res) => {
       handler(null, res);
