e4464d94726df2086c3d2e4afb80749b01470137
zengxingqi <me@zengxingqi.com>
2021-09-19 02:52:37 +0800

    feat: 屏幕分享

--- src/features/index.js
+++ src/features/index.js
@@ -2,7 +2,53 @@
  * @Author: 曾星旗 <me@zengxingqi.com>
  * @Date: 2021-09-19 02:08:50
  * @LastEditors: 曾星旗 <me@zengxingqi.com>
- * @LastEditTime: 2021-09-19 02:08:50
+ * @LastEditTime: 2021-09-19 02:42:51
  * @Description: 胶水代码
  * @FilePath: /like/src/features/index.js
  */
+import { getUserMedia } from "@/browser/media/devices/getUserMedia";
+import { getSources } from "@/native/modular/desktopCapturer";
+
+// 屏幕分享
+export function screenSharing() {
+  return getSources({
+    types: ["screen"],
+  })
+    .then(handlerSources)
+    .catch(handleError);
+}
+
+async function handlerSources(sources) {
+  for (const source of sources) {
+    if (source.name === "Entire Screen") {
+      try {
+        const stream = await getUserMedia({
+          audio: false,
+          video: {
+            mandatory: {
+              chromeMediaSource: "desktop",
+              chromeMediaSourceId: source.id,
+              minWidth: 1280,
+              maxWidth: 1280,
+              minHeight: 720,
+              maxHeight: 720,
+            },
+          },
+        });
+        handleStream(stream);
+      } catch (e) {
+        handleError(e);
+      }
+    }
+  }
+}
+
+function handleStream(stream) {
+  const video = document.querySelector("video");
+  video.srcObject = stream;
+  video.onloadedmetadata = () => video.play();
+}
+
+function handleError(e) {
+  console.log(e);
+}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,19 @@
   <div class="home">
     <h1>home</h1>
     <div>trtc-sdk版本号：{{ version }}</div>
+    <div>
+      <video autoplay playsinline id="video"></video>
+    </div>
+    <div>
+      <button @click.stop="screenSharing">屏幕分享</button>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import { getSDKVersion } from "@/sdk/";
+import { screenSharing } from "@/features/";
 export default {
   name: "Home",
   data() {
@@ -18,6 +25,11 @@ export default {
   components: {},
   created() {
     this.version = getSDKVersion();
+  },
+  methods: {
+    screenSharing() {
+      screenSharing();
+    },
   },
 };
 </script>
