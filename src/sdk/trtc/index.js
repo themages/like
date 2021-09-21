b68f8655a44e69b302960237c9825398f00ca31a
zengxingqi <me@zengxingqi.com>
2021-09-21 13:54:03 +0800

    feat: sdk封装

--- src/sdk/trtc/index.js
+++ src/sdk/trtc/index.js
@@ -2,13 +2,12 @@
  * @Author: 曾星旗 <me@zengxingqi.com>
  * @Date: 2021-09-20 22:55:25
  * @LastEditors: 曾星旗 <me@zengxingqi.com>
- * @LastEditTime: 2021-09-21 13:49:49
+ * @LastEditTime: 2021-09-21 13:53:57
  * @Description: 实例对象
  * @FilePath: /like/src/sdk/trtc/index.js
  */
 const TRTCCloud = window.require("trtc-electron-sdk").default;
 export const rtcCloud = new TRTCCloud();
-console.log("%O, $O", TRTCCloud, rtcCloud);
 // 获取 TRTCCloud 对象单例
 export function getTRTCShareInstance() {
   return rtcCloud.getTRTCShareInstance();
