e0957e6ab67bb23aab897a192000d599fe4fe581
zengxingqi <me@zengxingqi.com>
2021-10-21 15:04:15 +0800

    feat: mac schemes

--- vue.config.js
+++ vue.config.js
@@ -83,6 +83,7 @@ module.exports = {
           extendInfo: {
             NSCameraUsageDescription: "Request camera usage",
             NSMicrophoneUsageDescription: "Request mic usage",
+            CFBundleURLSchemes: ["likelive"],
           },
         },
         dmg: {
